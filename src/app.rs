e770012dcc03fb1debe9b197f384f18d5273b6e4
Glyphack <sh.hooshyari@gmail.com>
2025-08-17 12:15:01 +0200

    Refactor Gate structure

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,13 @@
 use egui::{
-    Align, Button, Color32, ImageSource, Layout, Pos2, Sense, Ui, Vec2, Widget, include_image, vec2,
+    Align, Button, Color32, Image, ImageSource, Layout, Pos2, Rect, Sense, Ui, Vec2, Widget,
+    include_image, vec2,
 };
+
+pub struct GateGraphics {
+    // TODO: Figure out what is the correct way to deal with images
+    pub svg: ImageSource<'static>,
+    pub pins: &'static [PinInfo],
+}
 
 #[derive(Debug, Clone, Copy)]
 pub enum PinKind {
@@ -14,64 +21,133 @@ pub struct PinInfo {
     pub offset: Vec2,
 }
 
-pub struct GateGraphics {
-    // TODO: Figure out what is the correct way to deal with images
-    pub svg: ImageSource<'static>,
-    pub pins: &'static [PinInfo],
-}
+pub static WIRE_GRAPHICS: GateGraphics = GateGraphics {
+    svg: include_image!("../assets/nand.svg"),
+    pins: &[],
+};
 
 pub static NAND_GRAPHICS: GateGraphics = GateGraphics {
     svg: include_image!("../assets/nand.svg"),
+    // offset must be made from the base_gate_size otherwise it will be unaligned when gates resize
     pins: &[
         PinInfo {
             kind: PinKind::Input,
-            offset: Vec2::new(-20.0, -7.0),
+            offset: Vec2::new(-19.0, -7.5),
         },
         PinInfo {
             kind: PinKind::Input,
-            offset: Vec2::new(-20.0, 7.0),
+            offset: Vec2::new(-19.0, 7.5),
         },
         PinInfo {
             kind: PinKind::Output,
-            offset: Vec2::new(24.0, 0.0),
+            offset: Vec2::new(19.0, 0.2),
         },
     ],
 };
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-pub enum LogicGate {
+pub enum Object {
     Nand,
+    Wire,
+}
+
+impl Object {
+    fn graphics(&self) -> &GateGraphics {
+        match self {
+            Object::Nand => &NAND_GRAPHICS,
+            Object::Wire => &WIRE_GRAPHICS,
+        }
+    }
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct GateInstance {
-    pub id: u32,
-    pub gate_type: LogicGate,
+    pub id: GateId,
+    pub gate_type: Object,
     pub position: Pos2,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct CanvasConfig {
+    pub base_gate_size: Vec2,
+    pub base_pin_size: f32,
+    pub base_input_pin_color: Color32,
+    pub base_output_pin_color: Color32,
+}
+
+impl Default for CanvasConfig {
+    fn default() -> Self {
+        Self {
+            base_gate_size: vec2(48.0, 46.0),
+            base_pin_size: 2.5,
+            base_input_pin_color: Color32::RED,
+            base_output_pin_color: Color32::GREEN,
+        }
+    }
+}
+
+impl GateInstance {}
+
+#[derive(
+    serde::Deserialize,
+    serde::Serialize,
+    Default,
+    Debug,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Hash,
+    Copy,
+)]
+pub struct GateId(u32);
+
+impl GateId {
+    pub fn incr(&mut self) {
+        self.0 += 1;
+    }
+}
+
+impl From<u32> for GateId {
+    fn from(v: u32) -> Self {
+        GateId(v)
+    }
+}
+
+impl Into<u32> for GateId {
+    fn into(self) -> u32 {
+        self.0
+    }
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Default)]
 #[serde(default)]
 pub struct TemplateApp {
     /// State
+    ///
     /// Gates placed on the canvas
     canvas_gates: Vec<GateInstance>,
     /// Next unique ID for gates
-    next_gate_id: u32,
+    next_gate_id: GateId,
 
     /// Dragging from panel
-    panel_dragged_gate: Option<LogicGate>,
+    panel_dragged_gate: Option<Object>,
     /// Temporary drag position if dragging palette-gate
     panel_drag_position: Option<Pos2>,
 
     /// Currently dragged gate id from canvas
-    dragged_canvas_gate: Option<u32>,
+    dragged_canvas_gate: Option<GateId>,
     /// Offset from mouse pointer to gate center at drag start
     drag_offset: Option<Vec2>,
+
+    /// Config
+    canvas_config: CanvasConfig,
 }
 
 impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Register all supported image loaders
         egui_extras::install_image_loaders(&cc.egui_ctx);
         if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
@@ -99,7 +175,7 @@ impl TemplateApp {
         let response = ui.add(egui::ImageButton::new(image).sense(Sense::click_and_drag()));
 
         if response.drag_started() {
-            self.panel_dragged_gate = Some(LogicGate::Nand);
+            self.panel_dragged_gate = Some(Object::Nand);
             self.panel_drag_position = None;
         }
         if response.dragged() {
@@ -125,7 +201,7 @@ impl TemplateApp {
         // handle dragging from panel
         if let (Some(gate_type), Some(pos)) = (&self.panel_dragged_gate, self.panel_drag_position) {
             if rect.contains(pos) {
-                Self::draw_gate_svg_with_pins(ui, gate_type, pos, Color32::YELLOW);
+                self.draw_gate_svg_with_pins(ui, gate_type, pos, Color32::YELLOW);
             }
         }
 
@@ -137,7 +213,7 @@ impl TemplateApp {
                     gate_type: gate_type.clone(),
                     position: pos,
                 });
-                self.next_gate_id += 1;
+                self.next_gate_id.incr();
                 self.panel_dragged_gate = None;
                 self.panel_drag_position = None;
             }
@@ -151,11 +227,11 @@ impl TemplateApp {
             && pointer_pressed
         {
             if let Some(mouse_pos) = pointer_pos {
-                for gate in self.canvas_gates.iter().rev() {
+                for gate in self.canvas_gates.iter() {
                     let size = Vec2::new(48.0, 36.0);
                     let gate_rect = egui::Rect::from_center_size(gate.position, size);
                     if gate_rect.contains(mouse_pos) {
-                        self.dragged_canvas_gate = Some(gate.id);
+                        self.dragged_canvas_gate = Some(gate.id.into());
                         self.drag_offset = Some(gate.position.to_vec2() - mouse_pos.to_vec2());
                         break;
                     }
@@ -186,27 +262,23 @@ impl TemplateApp {
             } else {
                 Color32::LIGHT_BLUE
             };
-            Self::draw_gate_svg_with_pins(ui, &gate.gate_type, gate.position, tint);
+            self.draw_gate_svg_with_pins(ui, &gate.gate_type, gate.position, tint);
         }
     }
 
-    fn draw_gate_svg_with_pins(ui: &mut Ui, gate_type: &LogicGate, pos: Pos2, _tint: Color32) {
-        let graphics = match gate_type {
-            LogicGate::Nand => &NAND_GRAPHICS,
-        };
-
-        let size = Vec2::new(48.0, 36.0);
-        let rect = egui::Rect::from_center_size(pos, size);
-        let image = egui::Image::new(graphics.svg.clone()).fit_to_exact_size(rect.size());
+    fn draw_gate_svg_with_pins(&self, ui: &mut Ui, gate: &Object, pos: Pos2, _tint: Color32) {
+        let rect = Rect::from_center_size(pos, self.canvas_config.base_gate_size);
+        let image = Image::new(gate.graphics().svg.clone()).fit_to_exact_size(rect.size());
         ui.put(rect, image);
 
-        for pin in graphics.pins {
+        for pin in gate.graphics().pins {
             let pin_pos = pos + pin.offset;
             let color = match pin.kind {
-                PinKind::Input => Color32::RED,
-                PinKind::Output => Color32::GREEN,
+                PinKind::Input => self.canvas_config.base_input_pin_color,
+                PinKind::Output => self.canvas_config.base_output_pin_color,
             };
-            ui.painter().circle_filled(pin_pos, 4.0, color);
+            ui.painter()
+                .circle_filled(pin_pos, self.canvas_config.base_pin_size, color);
         }
     }
 }
